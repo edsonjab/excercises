d2513521f2a657382be633ce8e955bdc769655c5
Edson Amaya <edsonjab@gmail.com>
2021-06-25 22:27:45 -0500

    Improve code for Array Rotation

--- ArrayRotation.js
+++ ArrayRotation.js
@@ -1,15 +1,16 @@
 // https://www.geeksforgeeks.org/array-rotation/
 
-const rotate = (arrayRotate, d, n) => {
+const rotate = (arrayRotate, d) => {
     const rotate = arrayRotate.splice(0, d);
-    rotate.forEach(element => {
-        arrayRotate.push(element);
-    });
+    arrayRotate = [...arrayRotate, ...rotate];
+    // rotate.forEach(element => {
+    //     arrayRotate.push(element);
+    // });
     return arrayRotate;
 }
 
 const arrayRotate = [1,2,3,4,5,6,7];
 const d = 4;
 
-const finalArray = rotate(arrayRotate, d, d);
+const finalArray = rotate(arrayRotate, d);
 console.log(finalArray);
