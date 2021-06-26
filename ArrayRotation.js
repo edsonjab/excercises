ff202383ad0b6b1279f1a8c28967d63a6c81081e
Edson Amaya <edsonjab@gmail.com>
2021-06-26 12:19:14 -0500

    Solve problem with One loop

--- ArrayRotation.js
+++ ArrayRotation.js
@@ -1,16 +1,24 @@
 // https://www.geeksforgeeks.org/array-rotation/
 
-const rotate = (arrayRotate, d) => {
-    const rotate = arrayRotate.splice(0, d);
-    arrayRotate = [...arrayRotate, ...rotate];
+const rotate = (arrayRotate, d, n) => {
+    const result = [];
+    let flag = 0;
+    for(let i = d; flag < n; i++,flag++) {
+        if(i===n)
+            i = 0;
+        result.push(arrayRotate[i]);
+    }
+    // const rotate = arrayRotate.splice(0, d);
+    // arrayRotate = [...arrayRotate, ...rotate];
     // rotate.forEach(element => {
     //     arrayRotate.push(element);
     // });
-    return arrayRotate;
+    return result;
 }
 
 const arrayRotate = [1,2,3,4,5,6,7];
 const d = 4;
+const n = 7;
 
-const finalArray = rotate(arrayRotate, d);
+const finalArray = rotate(arrayRotate, d, n);
 console.log(finalArray);
